6799cc41fe1a1d9400b93f3129cbf58aa2b7f4b6
mpm1 <mpm1@ualberta.ca>
2018-12-28 13:32:04 -0700

    Added a generic tileset.

---
+++ Assets/Scripts/Environment/EnvironmentCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentCamera : MonoBehaviour
+{
+    public GameObject environment;
+    public PlayerMovement player;
+    public bool followPlayer = false;
+    public float smoothing = 0.2f;
+
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 newPosition;
+        float zoom;
+        float z = transform.position.z;
+
+        if (followPlayer)
+        {
+            newPosition = player.transform.position;
+            zoom = 1.0f; //TODO: increase the camera size to show all ghosts.
+        }
+        else
+        {
+            newPosition = environment.transform.position;
+            zoom = Mathf.Max(environment.transform.lossyScale.x, environment.transform.lossyScale.y) / 2.0f;
+        }
+
+        newPosition.z = z;
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothing);
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoom, smoothing);
+    }
+}
---
+++ Assets/Scripts/Environment/EnvironmentGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnvironmentGenerator : MonoBehaviour
+{
+    private enum eDirection { East = 0, North, South, West };
+
+    public GameObject smallPellet;
+    public GameObject largePellet;
+    public int maxLargePellets = 6;
+    public int minLargePellets = 2;
+
+    public Vector2 ghostBoxScale;
+    public float ghostBoxDoorSize;
+
+    public Tilemap tileMap;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        int pelletCount = Mathf.RoundToInt(Random.Range(minLargePellets, maxLargePellets));
+
+        eDirection ghostDoor = GenerateGhostBox();
+    }
+
+    private eDirection GenerateGhostBox()
+    {
+        eDirection doorDirection = (eDirection)Mathf.RoundToInt(Random.Range(0, 3));
+
+        return doorDirection;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
